9c0806df79c0aa40f45860190eed12ba2ac244a8
Emanuele 'Adekro' Croce <e.croce88@gmail.com>
2023-05-08 14:44:21 +0200

    package component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./lib";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>Bottone</h2>
+      <Button>label</Button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/lib/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Button = ({ children }) => {
+  return (
+    <button>
+      <h4>{children}</h4>
+    </button>
+  );
+};
+export default Button;
