92b4a648931208a29fea16e0b86f298a00a7fa56
Emanuele 'Adekro' Croce <e.croce88@gmail.com>
2023-05-30 10:00:35 +0200

    add loadingspinner component

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,5 +6,15 @@ import Login from "./components/Login";
 import useLogin from "./hooks/useLogin";
 import Card from "./components/Card";
 import Input from "./components/Input/Input";
+import LoadingSpinner from "./components/LoadingSpinner";
 
-export { Button, Img, MessageModal, Login, useLogin, Card, Input };
+export {
+  Button,
+  Img,
+  MessageModal,
+  Login,
+  useLogin,
+  Card,
+  Input,
+  LoadingSpinner,
+};
